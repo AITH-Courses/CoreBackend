fbd0e16abe7b208ebbadb665c49a2c437ed4c1a4
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-04 23:41:49 +0300

    add redis

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ApplicationMode(Enum):
-
     """Variants of application mode."""
 
     DEV = "dev"
@@ -14,7 +13,6 @@ class ApplicationMode(Enum):
 
 
 class Config(BaseSettings):
-
     """Class for loading the necessary env variables."""
 
     POSTGRES_DB: str
@@ -22,6 +20,10 @@ class Config(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_PORT: str
     POSTGRES_HOST: str
+    REDIS_USER: str
+    REDIS_USER_PASSWORD: str
+    REDIS_PORT: str
+    REDIS_HOST: str
 
     MODE: ApplicationMode = Field(default=ApplicationMode.PRODUCTION)
 
@@ -51,6 +53,25 @@ class Config(BaseSettings):
         return self.__generate_asyncpg_db_url(
             self.POSTGRES_USER, self.POSTGRES_PASSWORD, self.POSTGRES_HOST,
             self.POSTGRES_PORT, self.POSTGRES_DB,
+        )
+
+    @staticmethod
+    def __get_redis_cache_url(user: str, password: str, host: str, port: str):
+        """Get redis cache url.
+
+        :param user:
+        :param password:
+        :param host:
+        :param port:
+        :return: dns
+        """
+        return f"redis://[[{user}]:[{password}]]@{host}:{port}/0"
+
+    @property
+    def cache_url(self) -> str:
+        """Get DSN to cache."""
+        return self.__get_redis_cache_url(
+            self.REDIS_USER, self.REDIS_USER_PASSWORD, self.REDIS_HOST, self.REDIS_PORT
         )
 
     model_config = SettingsConfigDict(env_file=".env")
