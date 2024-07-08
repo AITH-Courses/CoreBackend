25c96961c54680ad9078d8f2e5046bfe3ddfd28c
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-08 16:49:25 +0300

    add models and repository for course

---
+++ src/infrastructure/sqlalchemy/courses/unit_of_work.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.infrastructure.sqlalchemy.users.repository import SQLAlchemyUserRepository
+from src.services.courses.unit_of_work import CoursesUnitOfWork
+
+
+class SQLAlchemyCoursesUnitOfWork(CoursesUnitOfWork):
+
+    """SQLA implementation for unit of work."""
+
+    def __init__(self, sqla_session: AsyncSession) -> None:
+        self.session = sqla_session
+        self.user_repo = SQLAlchemyUserRepository(sqla_session)
+        self.course_repo = SQLAlchemyUserRepository(sqla_session)
+
+    async def begin(self) -> None:
+        await self.session.begin()
+
+    async def commit(self) -> None:
+        await self.session.commit()
+
+    async def rollback(self) -> None:
+        await self.session.rollback()
