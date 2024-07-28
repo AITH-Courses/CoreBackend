1847da92f65c861543d2699f3ef98422e0505189
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-28 16:53:43 +0300

    add feedback models and sqla repository

---
+++ src/infrastructure/sqlalchemy/feedback/models.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import datetime
+import uuid
+
+from sqlalchemy import ForeignKey, Text, text, Date
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.domain.feedback.entities import FeedbackEntity
+from src.domain.feedback.value_objects import FeedbackText, Vote
+from src.infrastructure.sqlalchemy.session import Base
+
+
+class Feedback(Base):
+
+    """SQLAlchemy model of Feedback."""
+
+    __tablename__ = "feedbacks"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    course_id: Mapped[uuid.UUID] = mapped_column(nullable=False)
+    author_id: Mapped[uuid.UUID] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    votes: Mapped[list[VoteForFeedback]] = relationship(back_populates="feedback")
+    date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
+
+    is_archive: Mapped[bool] = mapped_column(nullable=False, default=False)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow,
+    )
+
+    @staticmethod
+    def from_domain(feedback: FeedbackEntity) -> Feedback:
+        return Feedback(
+            id=uuid.UUID(feedback.id),
+            course_id=feedback.course_id,
+            author_id=feedback.author_id,
+            content=feedback.text.value,
+            date=feedback.date,
+            votes=[
+                VoteForFeedback(feedback_id=uuid.UUID(feedback.id), user_id=uuid.UUID(vote.user_id), vote_type=vote.vote_type)
+                for vote in feedback.votes
+            ],
+        )
+
+    def to_domain(self) -> FeedbackEntity:
+        return FeedbackEntity(
+            id=str(self.id),
+            course_id=str(self.course_id),
+            author_id=str(self.author_id),
+            text=FeedbackText(self.content),
+            votes={Vote(user_id=str(vote.user_id), vote_type=vote.vote_type) for vote in self.votes},
+            date=self.date,
+        )
+
+
+class VoteForFeedback(Base):
+
+    """SQLAlchemy model of Vote for feedback."""
+
+    __tablename__ = "feedback_votes"
+
+    feedback_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("feedbacks.id"), primary_key=True)
+    user_id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    vote_type: Mapped[str] = mapped_column(nullable=False)
+    feedback: Mapped[Feedback] = relationship(back_populates="votes")
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+    )
+
+    @staticmethod
+    def from_domain(vote: Vote, feedback_id: str) -> VoteForFeedback:
+        return VoteForFeedback(
+           feedback_id=uuid.UUID(feedback_id),
+           user_id=uuid.UUID(vote.user_id),
+           vote_type=vote.vote_type
+        )
