0dea22ab03b0429f3eaf794a1f8edf1a706cf7ca
Daniil-Solo <daniil.solo1723@gmail.com>
2024-08-29 21:32:18 +0300

    add exceptions for talent profile

--- src/domain/talent_profile/entities.py
+++ src/domain/talent_profile/entities.py
@@ -15,6 +15,7 @@ class TalentProfileEntity:
     """Entity of talent profile."""
 
     id: UUID
+    image_url: str | None = field(default=None)
     location: str = field(default="")
     position: str = field(default="")
     company: str = field(default="")
@@ -22,3 +23,18 @@ class TalentProfileEntity:
     link_eng_resume: EmptyLinkValueObject = field(default=EmptyLinkValueObject(""))
     link_tg_personal: EmptyLinkValueObject = field(default=EmptyLinkValueObject(""))
     link_linkedin: EmptyLinkValueObject = field(default=EmptyLinkValueObject(""))
+
+    def update_profile(self, image_url: str | None, location: str, position: str, company: str) -> None:
+        self.image_url = image_url
+        self.location = location
+        self.position = position
+        self.company = company
+
+    def update_links(
+            self, link_ru_resume: EmptyLinkValueObject, link_eng_resume: EmptyLinkValueObject,
+            link_tg_personal: EmptyLinkValueObject, link_linkedin: EmptyLinkValueObject,
+    ) -> None:
+        self.link_ru_resume = link_ru_resume
+        self.link_eng_resume = link_eng_resume
+        self.link_tg_personal = link_tg_personal
+        self.link_linkedin = link_linkedin
