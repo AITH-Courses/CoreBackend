3b5811b5bd12171faefc24093d085de94397d34a
Daniil-Solo <daniil.solo1723@gmail.com>
2024-09-16 10:46:53 +0500

    correct imports

--- src/api/admin/group_google_calendar/integration_router.py
+++ src/api/admin/group_google_calendar/integration_router.py
@@ -4,8 +4,8 @@ from fastapi.responses import JSONResponse
 from src.api.admin.courses.dependencies import get_admin
 from src.api.admin.group_google_calendar.dependencies import get_group_google_calendar_service
 from src.api.admin.group_google_calendar.schemas import (
-    UpdateManyGroupGoogleCalendarMessage,
-    UpdateManyGroupGoogleCalendarsRequest,
+    UpdateCourseGroupGoogleCalendarsRequest,
+    UpdateCourseGroupGoogleCalendarMessageResponse,
 )
 from src.domain.auth.entities import UserEntity
 from src.services.group_google_calendar.command_service import GroupGoogleCalendarCommandService
@@ -17,20 +17,20 @@ router = APIRouter(prefix="/integrations/google_calendar_links", tags=["integrat
 @router.post(
     "",
     status_code=status.HTTP_200_OK,
-    description="Add google calendar links for many courses. The group name is optional, it is required if there is "
+    description="Add google calendar links for one course. The group name is optional, it is required if there is "
                 "more than one group",
     summary="Add google calendar links",
     responses={
         status.HTTP_200_OK: {
-            "model": list[UpdateManyGroupGoogleCalendarMessage],
+            "model": UpdateCourseGroupGoogleCalendarMessageResponse,
             "description": "Messages explained processing. OK is standard message after success processing",
         },
     },
-    response_model=list[UpdateManyGroupGoogleCalendarMessage],
+    response_model=UpdateCourseGroupGoogleCalendarMessageResponse,
 )
 async def update_google_calendar_links(
     _: UserEntity = Depends(get_admin),
-    data: UpdateManyGroupGoogleCalendarsRequest = Body(),
+    data: UpdateCourseGroupGoogleCalendarsRequest = Body(),
     command_service: GroupGoogleCalendarCommandService = Depends(get_group_google_calendar_service),
 ) -> JSONResponse:
     """Update google calendar links.
@@ -41,5 +41,5 @@ async def update_google_calendar_links(
     message = await command_service.update(course, data.course_run_name)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content=UpdateManyGroupGoogleCalendarMessage(message=message).model_dump(mode="json"),
+        content=UpdateCourseGroupGoogleCalendarMessageResponse(message=message).model_dump(mode="json"),
     )
